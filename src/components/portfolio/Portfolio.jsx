01335555e19b16ea183c4b23011a52c730ac73fb
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-06 23:03:44 +0700

    home page & components

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './portfolio.css'
+const Portfolio = () => {
+    return (
+        <div id='portfolio'>Portfolio</div>
+      )
+}
+export default Portfolio
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './resume.css'
+const Resume = () => {
+    return (
+        <div id='resume'>Resume</div>
+      )
+}
+export default Resume
