d4afc3ef00eea5f5f749907579a8e80fec5d7195
Dwi Wjiaya <11dwiwijaya@gmail.com>
2024-03-07 16:00:34 +0700

    works components

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
-import './portfolio.css'
+import React, { useState } from "react";
+import "./portfolio.css";
+import Menu from "./Menu";
+
 const Portfolio = () => {
-    return (
-        <div id='portfolio'>Portfolio</div>
-      )
-}
-export default Portfolio
+  const [items, setItems] = useState(Menu);
+  const filterItems = (categoryItem) => {
+    const updatedItems = Menu.filter((curElem) => {
+      return curElem.category === categoryItem;
+    });
+    setItems(updatedItems);
+  };
+  return (
+    <section className="work container section" id="resume">
+      <h2 className="section__title">Recent Works</h2>
+
+      <div className="work__filters">
+        <span className="work__item" onClick={() => setItems(Menu)}>Everythings</span>
+        <span className="work__item" onClick={() => filterItems('Creative')}>Creative</span>
+        <span className="work__item" onClick={() => filterItems('Art')}>Art</span>
+        <span className="work__item" onClick={() => filterItems('Design')}>Design</span>
+        <span className="work__item" onClick={() => filterItems('Photography')}>Photography</span>
+      </div>
+
+      <div className="work__container grid">
+        {items.map((elem) => {
+          const { id, image, title, category } = elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__tumbnail">
+                <img src={image} alt="" className="work__img" />
+                <div className="work__mask"></div>
+              </div>
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href="#" className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+export default Portfolio;
