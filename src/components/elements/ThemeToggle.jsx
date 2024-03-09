aedc86b17e534994eadfca450243d3d9f072faeb
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-09 19:27:32 +0700

    feat: dark mode feature with toggle functionality

---
+++ src/components/elements/ThemeToggle.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import LightIcon from '../../assets/theme-light.svg';
+import DarkIcon from '../../assets/theme-dark.svg';
+
+const ThemeToggle = () => {
+    const [theme, setTheme] = useState(() => {
+        // Check if the theme mode has been saved in local storage
+        const savedMode = localStorage.getItem('themeMode');
+        return savedMode ? JSON.parse(savedMode) : false;
+    });
+
+    useEffect(() => {
+        // Save the theme preference to local storage
+        localStorage.setItem('themeMode', JSON.stringify(theme));
+
+        // Change the data-theme attribute on the <body> element
+        document.body.setAttribute('data-theme', theme ? 'dark' : 'light');
+    }, [theme]);
+
+    const themeToggle = () => {
+        setTheme(prevMode => !prevMode);
+    };
+
+    return (
+        <button className='theme__toggle' onClick={themeToggle}>
+            <img src={theme ? DarkIcon : LightIcon} alt="Theme Icon" />
+        </button>
+    );
+};
+
+export default ThemeToggle;
