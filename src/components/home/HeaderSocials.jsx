815ad2323d8dd5289a58413be7387edc3b785e24
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-10 12:02:31 +0700

    fix: home socials section

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-const socials = ['instagram', 'twitter', 'behance', 'dribbble', 'pinterest', 'facebook'];
+const socials = ['instagram', 'twitter', 'behance', 'dribbble', 'github', 'facebook'];
 
 const HeaderSocials = () => {
     return (
         <div className="home__socials">
             {socials.map((social, index) => (
                 <a aria-label={`go to ${social}`} key={index} href={`https://${social}.com`} target='_blank' className="home__social-link">
-                    <i className={`fa-brands fa-${social}`}></i>
+                    <i className={`bx bx-tada bxl-${social}`}></i>
                 </a>
             ))}
         </div>
