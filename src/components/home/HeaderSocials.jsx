83bca16dd57138f4336ac079c93f4cd6b4231191
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-11 09:48:03 +0700

    revert: boxicon using cdn font

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 const socials = ['instagram', 'twitter', 'behance', 'dribbble', 'github', 'facebook'];
-import 'boxicons'
 
 const HeaderSocials = () => {
     return (
         <div className="home__socials">
             {socials.map((social, index) => (
                 <a aria-label={`go to ${social}`} key={index} href={`https://${social}.com`} target='_blank' className="home__social-link">
-                    <box-icon name={social} type='logo' animation='tada' ></box-icon>
+                    <i className={`bx bxl-${social}`}></i>
                 </a>
             ))}
         </div>
