4e5be8d16f3478e8938ea030619abaed2363fce9
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-03-12 12:00:28 +0700

    style: redesign servies component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import "./services.css";
-import Image1 from "../../assets/service-1.svg";
-import Image2 from "../../assets/service-2.svg";
-import Image3 from "../../assets/service-3.svg";
+import icon1 from "../../assets/service-1.svg";
+import icon2 from "../../assets/service-2.svg";
+import icon3 from "../../assets/service-3.svg";
 
 const data = [
   {
-    id: 1,
-    image: Image1,
-    title: "UI/UX design",
-    description:
-      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
-  },
-  {
     id: 2,
-    image: Image2,
+    icon: 'code-block',
     title: "Web Development",
     description:
-      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+      "Build dynamic and responsive websites tailored to your needs, ensuring seamless functionality and optimal performance.",
+  },
+  {
+    id: 1,
+    icon: 'palette',
+    title: "UI/UX design",
+    description:
+      "Craft visually stunning and user-friendly interfaces that captivate your audience and enhance user experience.",
   },
   {
     id: 3,
-    image: Image3,
-    title: "Photography",
+    icon: 'pen',
+    title: "Design",
     description:
-      "Lorem ipsum dolor sit amet consectetuer adipiscing elit         aenean commodo ligula eget.",
+      "Create captivating visuals and branding materials that leave a lasting impression on your audience.",
+  },
+  {
+    id: 4,
+    icon: 'chat',
+    title: "Others",
+    description:
+      "Discuss any other specific needs or services you require, let's tailor a solution to meet your unique requirements.",
   },
 ];
 
@@ -35,10 +42,10 @@ const Services = () => {
         <h2 className="section__title">Services</h2>
       </div>
       <div className="services__container grid">
-        {data.map(({ id, image, title, description }) => {
+        {data.map(({ id, icon, title, description }) => {
           return (
             <div className="services__card" key={id}>
-              <img src={image} alt="service" className="services__img" />
+              <i className={`services__icon bx bx-${icon}`} ></i>
               <h3 className="services__title">{title}</h3>
               <p className="services__description">{description}</p>
             </div>
