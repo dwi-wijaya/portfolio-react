ad674ffa900fea6ce934063f88fd4e5c50d70f4f
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-16 13:06:27 +0700

    redesign: homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,33 +3,57 @@ import React, { useEffect } from 'react';
 import './home.css'
 import Me from '../../assets/avatar-1.svg'
 import HeaderSocials from "./HeaderSocials";
-import ScrollDown from "./ScrollDown";
 import Shapes from "./Shapes";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
     return (
-        <section data-section className="home container" id="home">
-            <div className="intro">
-                <img
-                    width={108}
-                    height={108} 
-                    fetchPriority="high"
-                    src={Me}
-                    alt="Avatar Dwi"
-                    className="home__img"
-                />
-                <h1 className="home__name">
-                    Dwi Wijaya
-                </h1>
-                <span className="home__education">
-                    I'm a Full-stack developer
-                </span>
-                <HeaderSocials />
-                <a aria-label='hire me' href="#contact" className="btn">Hire Me</a>
+        <>
+            <div className="header__wrapper">
+                <Shapes />
             </div>
-            <Shapes />
-        </section>
+            <section data-section className="home container" id="home">
+                <div className="home-section">
+                    <div className="intro">
+                        <img
+                            width={108}
+                            height={108}
+                            src={Me}
+                            alt="Avatar Dwi"
+                            className="home__img"
+                        />
+                        <div className="home__greeting">
+                            <h1 className="home__name">
+                                Hi, Dwi Wijaya
+                            </h1>
+                            <div className="animate-waving-hand">👋</div>
+                        </div>
+                        <span className="home__role">
+                            I&apos;m a Full-stack developer <i className="bx bx-code-alt"></i> &nbsp;
+                        </span>
+                        <HeaderSocials />
+                        <hr className="border" />
+                        <div className='card'>
+                            <div className='contact__header'>
+                                <i className='bx bx-rocket'></i>
+                                <h3 className='text-xl font-medium'>Lets work together!</h3>
+                            </div>
+                            <p className='contact__content'>
+                                I&apos;m open for freelance projects, feel free to email me to see how
+                                can we collaborate.
+                            </p>
+                            <Link to='contact'
+                                aria-label='contact me'
+                                className='btn'
+                            >
+                                Contact me
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 export default Home
