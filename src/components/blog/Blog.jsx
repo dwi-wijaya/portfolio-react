00f1e29a1118b24c334aabcd5e150a3a1874c98b
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-16 17:48:45 +0700

    blogpage & component

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,38 +1,15 @@
-import React from 'react';
-import './blog.css'
-import Card from './Card';
-import Data from './Data';
-import PageHeading from '../elements/PageHeading';
-const Blog = () => {
-  return (
-    <section data-section className="blog container section" id="blog">
-      <PageHeading
-        title='Latest Posts'
-        description='Stay updated with my recent thoughts, insights, and more.'
-      />
+import React from 'react'
+import PageHeading from '../elements/PageHeading'
 
-      <div className="blog__container">
-        {Data.map((data, id) => {
-          return (
-            <Card
-              key={id}
-              thumb={data.thumb}
-              title={data.title}
-              date={data.date}
-              author={data.author}
-            />
-          )
-        })}
-        <div className="blog__card">
-          <div className="blog__card-more">
-            <a aria-label='see more articles' href='#' className="see-more">
-              View All Blogs
-              <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="22" width="22" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"></path></svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+const Blog = () => {
+    return (
+        <section data-section className="blog container section" id="blog">
+            <PageHeading title='Blog' description='Exploring the world of code, creativity, and constant learning.' />
+            <div className="blog__container">
+                <h3><i className='bx bx-time-five'></i> Coming Soon</h3>
+            </div>
+        </section>
+    )
 }
+
 export default Blog
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Blog from '../components/blog/Blog'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <Blog/>
   )
 }
 
