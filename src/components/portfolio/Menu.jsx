0b6746b0100bb2dcd0da44496578b286d79d89ff
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-17 21:34:58 +0700

    style: redesign project pages

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -8,39 +8,51 @@ import Work6 from "../../assets/work-6.svg";
 const Menu = [
   {
     id: 1,
+    isFeatured: true,
     image: Work1,
     title: "Project Management Illustration",
-    category: "Design",
+    category: "code",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
   {
     id: 2,
     image: Work2,
+    isFeatured: true,
     title: "Guest App Walkthrough Screens",
-    category: "Art",
+    category: "code",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
   {
     id: 3,
+    isFeatured: false,
     image: Work3,
     title: "Delivery App Wireframe",
-    category: "Branding",
+    category: "design",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
   {
     id: 4,
+    isFeatured: false,
     image: Work4,
     title: "Onboarding Motivation",
-    category: "Design",
+    category: "uiux",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
   {
     id: 5,
     image: Work5,
+    isFeatured: false,
     title: "iMac Mockup Design",
-    category: "Creative",
+    category: "design",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
   {
     id: 6,
     image: Work6,
+    isFeatured: false,
     title: "Game Store App Concept",
-    category: "Art",
+    category: "others",
+    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
   },
 ];
 export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,46 +2,61 @@ import React, { useState } from "react";
 import "./portfolio.css";
 import Menu from "./Menu";
 import PageHeading from "../elements/PageHeading";
-
+import Category from "./Category";
+import Pin from '../../assets/pin.svg'
 const Portfolio = () => {
   const [items, setItems] = useState(Menu);
-  const [activeCategory, setActiveCategory] = useState('Everythings');
+  const [activeCategory, setActiveCategory] = useState('all');
+
   const filterItems = (categoryItem) => {
-    const updatedItems = Menu.filter((curElem) => {
-      return curElem.category === categoryItem;
-    });
-    setItems(updatedItems);
+    if (categoryItem === 'all') {
+      setItems(Menu);
+    } else {
+      const updatedItems = Menu.filter((curElem) => {
+        return curElem.category === categoryItem;
+      });
+      setItems(updatedItems);
+    }
     setActiveCategory(categoryItem);
   };
+
+  const IconCategory = {
+    code: 'bx bx-code',
+    uiux: 'bx bx-palette',
+    design: 'bx bx-pen',
+    others: 'bx bx-customize',
+  };
+
   return (
     <section className="work container section" id="portfolio" data-aos='fade-up'>
       <PageHeading
-        title='Recents Works'
+        title='My Works'
         description='Explore my latest projects and creations.'
       />
 
-      <div className="work__filters">
-        <span className={`work__item ${activeCategory === 'Everythings' ? 'active' : ''}`} onClick={() => { setItems(Menu); setActiveCategory('Everythings'); }}>Everythings</span>
-        <span className={`work__item ${activeCategory === 'Creative' ? 'active' : ''}`} onClick={() => filterItems('Creative')}>Creative</span>
-        <span className={`work__item ${activeCategory === 'Art' ? 'active' : ''}`} onClick={() => filterItems('Art')}>Art</span>
-        <span className={`work__item ${activeCategory === 'Design' ? 'active' : ''}`} onClick={() => filterItems('Design')}>Design</span>
-        <span className={`work__item ${activeCategory === 'Photography' ? 'active' : ''}`} onClick={() => filterItems('Photography')}>Photography</span>
-      </div>
+      <Category filter={filterItems} active={activeCategory} />
 
       <div className="work__container grid">
         {items.map((elem) => {
-          const { id, image, title, category } = elem;
+          const { id, image, title, category, isFeatured,desc } = elem;
           return (
-            <div className="work__card" key={id}>
-              <div className="work__tumbnail">
-                <img src={image} alt="" className="work__img" />
-                <div className="work__mask"></div>
+            <div data-aos='fade-right' className="work__card" key={id}>
+              <div className="work__thumbnail">
+                {isFeatured && <div className="featured"><img src={Pin} alt="" srcSet="" />Featured </div>}
+                <span className="work__category"><i className={IconCategory[category]}></i></span>
+                <div className="relative">
+                  <img src={image} alt="" className="work__img" />
+                  <div className="work__mask">
+                    View Project<i className='bx bx-right-arrow-alt'></i>
+                  </div>
+                </div>
               </div>
-              <span className="work__category">{category}</span>
-              <h3 className="work__title">{title}</h3>
-              <a aria-label="demo" href="#" className="work__button">
-                <i className="icon-link work__button-icon"></i>
-              </a>
+              <div className="work__desc">
+                <h3 className="work__title">{title}</h3>
+                <p className="work__subtitle">
+                  {desc}
+                </p>
+              </div>
             </div>
           );
         })}
@@ -49,4 +64,5 @@ const Portfolio = () => {
     </section>
   );
 };
+
 export default Portfolio;
