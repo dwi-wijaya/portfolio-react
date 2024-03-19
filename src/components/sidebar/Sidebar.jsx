a1b8e6fc77562f88d19fa9e339575ab2f4404dcd
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-19 17:24:30 +0700

    revert: sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
 import LogoDark from '../../assets/logo-dark.svg';
 import LogoLight from '../../assets/logo-light.svg';
@@ -9,31 +9,22 @@ import { MENU_ITEMS } from '../../constants/menu';
 
 const Sidebar = () => {
     const { theme } = useTheme();
-    const [toggle, setToggle] = useState(false);
-    const sidebarRef = useRef(null);
+    const [toggle, showMenu] = useState(false);
 
-    const handleClickOutside = (event) => {
-        if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-            setToggle(false);
-        }
-    };
-    document.addEventListener("mousedown", handleClickOutside);
-
-    const handleToggleClick = () => {
-        setToggle(prevToggle => !prevToggle);
-    };
+    // Define your navigation items and icons using an object
+    
 
     return (
         <>
-            <aside ref={sidebarRef} className={`aside ${toggle && 'show-menu'}`}>
-                <Link aria-label='go home' to="/" className="nav__logo">
-                    <img src={theme === 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
+            <aside className={`aside ${toggle && 'show-menu'}`}>
+                <Link aria-label='go home' to="" className="nav__logo">
+                    <img src={theme == 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
                 </Link>
                 <nav className="nav">
                     <div className="nav__menu">
                         <ul className="nav__list">
                             {MENU_ITEMS.map((item, index) => (
-                                <li key={index} className='nav__item'>
+                                <li key={index} className='nav__item '>
                                     <NavLink to={`${item.href}`} title={item.label} aria-label={`go to ${item.href}`} className="nav__link">
                                         <i className={item.iconClass}></i>
                                     </NavLink>
@@ -45,10 +36,10 @@ const Sidebar = () => {
                 <div className="nav__footer">
                     <span className="copyright">&copy; {new Date().getFullYear()}</span>
                 </div>
-                <div onClick={handleToggleClick} className={`toggle nav__toggle ${toggle ? 'nav__toggle-open' : ''}`}>
-                    <i className="icon-menu"></i>
-                </div>
             </aside>
+            <div onClick={() => showMenu(!toggle)} className={`toggle sidebar__toggle ${toggle ? 'sidebar__toggle-open' : ''}`}>
+                <i className="icon-menu"></i>
+            </div>
         </>
     );
 };
