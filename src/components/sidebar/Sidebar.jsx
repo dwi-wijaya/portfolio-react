f689649e269ff532de134d14eba848b1ab179ae1
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-03-20 04:52:16 +0700

    hook close sidebar when click outisde

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './sidebar.css';
 import LogoDark from '../../assets/logo-dark.svg';
 import LogoLight from '../../assets/logo-light.svg';
@@ -9,15 +9,23 @@ import { MENU_ITEMS } from '../../constants/menu';
 
 const Sidebar = () => {
     const { theme } = useTheme();
-    const [toggle, showMenu] = useState(false);
+    const [toggle, setToggle] = useState(false);
     useEffect(() => {
         document.body.setAttribute('class', toggle ? 'sidebar-expanded' : '');
         document.body.setAttribute('class', toggle ? 'sidebar-expanded' : '');
     }, [toggle]);
-    
+    const sidebarRef = useRef(null);
+
+    const handleClickOutside = (event) => {
+        if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+            setToggle(false);
+        }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
     return (
         <>
-            <aside className={`aside ${toggle && 'show-menu'}`}>
+            <aside ref={sidebarRef} className={`aside ${toggle && 'show-menu'}`}>
                 <Link aria-label='go home' to="" className="nav__logo">
                     <img src={theme == 'dark' ? LogoDark : LogoLight} alt="Dwi-logo" />
                 </Link>
@@ -37,10 +45,10 @@ const Sidebar = () => {
                 <div className="nav__footer">
                     <span className="copyright">&copy; {new Date().getFullYear()}</span>
                 </div>
+                <div onClick={() => setToggle(!toggle)} className={`toggle sidebar__toggle ${toggle ? 'sidebar__toggle-open' : ''}`}>
+                    <i className="icon-menu"></i>
+                </div>
             </aside>
-            <div onClick={() => showMenu(!toggle)} className={`toggle sidebar__toggle ${toggle ? 'sidebar__toggle-open' : ''}`}>
-                <i className="icon-menu"></i>
-            </div>
         </>
     );
 };
