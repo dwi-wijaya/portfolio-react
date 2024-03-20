e2396fa11c8af3e235a2b7c117a56a8131d16940
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-03-20 14:43:34 +0700

    fix: dashboard leetcode acceptance rate percentage

--- src/components/dashboard/letcode/Letcode.jsx
+++ src/components/dashboard/letcode/Letcode.jsx
@@ -5,7 +5,7 @@ const Letcode = ({ data }) => {
   return (
     <div className="dashboard__leetcode">
       <div className="dashboard__cards">
-        <DashboardCard title='Acceptance Rate' value={data.acceptanceRate} />
+        <DashboardCard title='Acceptance Rate' value={data.acceptanceRate} unit='%' />
         <DashboardCard title='Contribution Points' value={data.contributionPoints} />
         <DashboardCard title='Total Solved' value={data.totalSolved} />
         <DashboardCard title='Easy - Medium - Hard' value={[data.easySolved, data.mediumSolved, data.hardSolved]} isMultiple={true} />
